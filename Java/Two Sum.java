d475967c82b2ac06fbce5505d1950ec88c09ef11
Saomya Tangri <44325696+saomyatangri@users.noreply.github.com>
2026-01-10 21:06:57 -0500

    Update Two Sum.java

--- Java/Two Sum.java
+++ Java/Two Sum.java
@@ -1,18 +1,38 @@
+/* My new solution, only one pass of the array and more professional output handling */
+import java.util.HashMap;
+ 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
+            Integer wantedKey = map.get(target-nums[i]);
+            if (wantedKey != null) {
+                int[] output = {wantedKey, i};
+                return output;
+            }
             map.put(nums[i], i);
         }
-        int result[] = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            int goal = target - nums[i];
-            if (map.containsKey(goal) && map.get(goal) != i) {
-                result[0] = i;
-                result[1] = map.get(goal);
-                return result;
-            }
-        }
-        return result;
-    }
+        throw new IllegalArgumentException("No two sum solution");
+    }    
 }
+
+/* My previous solution, not as efficient but still has the same O(N) as the new one  */
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             map.put(nums[i], i);
+//         }
+//         int result[] = new int[2];
+//         for (int i = 0; i < nums.length; i++) {
+//             int goal = target - nums[i];
+//             if (map.containsKey(goal) && map.get(goal) != i) {
+//                 result[0] = i;
+//                 result[1] = map.get(goal);
+//                 return result;
+//             }
+//         }
+//         return result;
+//     }
+// }
