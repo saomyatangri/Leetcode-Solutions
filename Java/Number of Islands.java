63c59115223320b41b72983c1d15ea2b567a493a
Saomya Tangri <44325696+saomyatangri@users.noreply.github.com>
2026-01-18 18:10:23 -0500

    Update Number of Islands.java

--- Java/Number of Islands.java
+++ Java/Number of Islands.java
@@ -43,7 +43,7 @@ class Solution {
     }
 }
 
-/* My previous solution, DFS */
+/* My previous solution, also BFS */
 class Solution2 {
     public int numIslands(char[][] grid) {
         if (grid.length == 0) return 0;
@@ -54,15 +54,15 @@ class Solution2 {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1' && !checked[i][j] ) {
                     islands++;
-                    //System.out.printf("Calling DFS, islands = %d, i = %d, j = %d\n", islands, i, j);
-                    DFS(q, grid, checked, i, j);
+                    //System.out.printf("Calling BFS, islands = %d, i = %d, j = %d\n", islands, i, j);
+                    BFS(q, grid, checked, i, j);
                 }
             }
         }
         return islands;
     }
     
-    private void DFS(Queue<int[]> q, char[][] grid, boolean[][] checked, int i, int j) {
+    private void BFS(Queue<int[]> q, char[][] grid, boolean[][] checked, int i, int j) {
         int[] temp = {i, j};
         q.add(temp);
         while(!q.isEmpty()) {
@@ -71,19 +71,19 @@ class Solution2 {
             if (!checked[x][y]) {
                 checked[x][y] = true;
                 if (x+1 < grid.length && grid[x+1][y] == '1' && !checked[x+1][y]) {
-                    //System.out.printf("inside DFS 1, old = %d,%d new = %d,%d\n", x, y, x+1, y);
+                    //System.out.printf("inside BFS 1, old = %d,%d new = %d,%d\n", x, y, x+1, y);
                     int[] temp1 = {x+1, y};
                     q.add(temp1);
                 } if (x-1 >= 0 && grid[x-1][y] == '1' && !checked[x-1][y]) {
-                    //System.out.printf("inside DFS 2, old = %d,%d new = %d,%d\n", x, y, x-1, y);
+                    //System.out.printf("inside BFS 2, old = %d,%d new = %d,%d\n", x, y, x-1, y);
                     int[] temp1 = {x-1, y};
                     q.add(temp1);
                 } if (y+1 < grid[0].length && grid[x][y+1] == '1' && !checked[x][y+1]) {
-                    //System.out.printf("inside DFS 3, old = %d,%d new = %d,%d\n", x, y, x, y+1);
+                    //System.out.printf("inside BFS 3, old = %d,%d new = %d,%d\n", x, y, x, y+1);
                     int[] temp1 = {x, y+1};
                     q.add(temp1);
                 } if (y-1 >= 0 && grid[x][y-1] == '1' && !checked[x][y-1]) {
-                    //System.out.printf("inside DFS 4, old = %d,%d new = %d,%d\n", x, y, x, y-1);
+                    //System.out.printf("inside BFS 4, old = %d,%d new = %d,%d\n", x, y, x, y-1);
                     int[] temp1 = {x, y-1};
                     q.add(temp1);
                 }
