011f89c34e2860c536c59484408198b7db1ec131
Saomya Tangri <44325696+saomyatangri@users.noreply.github.com>
2026-01-27 22:56:18 -0500

    Create Find if Path Exists in Graph.java

---
+++ Java/Find if Path Exists in Graph.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        Map<Integer, Set<Integer>> adjacencyList = new HashMap<>();
+        Set<Integer> visited = new HashSet<>();
+
+        for (int k = 0; k < n; k++) {
+            Set<Integer> temp = new HashSet<>();
+            adjacencyList.put(k, temp);
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            adjacencyList.get(u).add(v);
+            adjacencyList.get(v).add(u);
+        }
+
+        return DFS(adjacencyList, visited, source, destination, n);
+    }
+
+    private boolean DFS(Map<Integer, Set<Integer>> adjacencyList, Set<Integer> visited, int source, int destination, int n) {
+        if (source == destination) {
+            //success case, path exists
+            return true;
+        }
+        visited.add(source);
+        for (Integer node : adjacencyList.get(source)) {
+            if (!visited.contains(node)) {
+                if (DFS(adjacencyList, visited, node, destination, n)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
